28adb30166eb66eeed09a8671886f9f6208fe693
lettuce <lettuce.lu@lettucelu.com>
2023-05-12 23:57:13 +0800

    練習Django admin 後台的使用

--- myBackupApp/admin.py
+++ myBackupApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserEntity
 
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'age', 'phone')
+    list_per_page = 3
 # Register your models here.
+admin.site.register(UserEntity, UserAdmin)
--- myBackupApp/models.py
+++ myBackupApp/models.py
@@ -7,8 +7,11 @@ class UserEntity(models.Model):
     age = models.IntegerField(default=0)
     phone = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
 
     class Meta:
         # 配置表名
         db_table = 't_user'
-
+        verbose_name = '用戶管理'
+        verbose_name_plural = verbose_name

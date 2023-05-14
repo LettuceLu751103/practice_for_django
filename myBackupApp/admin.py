18bd68b2790c1665f8128f3546c7b333a42d4e35
lettuce.lu <lettuce.lu@lettucelu.com>
2023-05-15 01:08:30 +0800

    add CategoryEntity, DeviceEntity

--- myBackupApp/admin.py
+++ myBackupApp/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from .models import UserEntity
+from .models import UserEntity, CategoryEntity, DeviceEntity
 
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'age', 'phone')
     list_per_page = 3
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'vendor', 'category')
+
 # Register your models here.
 admin.site.register(UserEntity, UserAdmin)
+admin.site.register(CategoryEntity, CategoryAdmin)
+admin.site.register(DeviceEntity, DeviceAdmin)
--- myBackupApp/models.py
+++ myBackupApp/models.py
@@ -15,3 +15,34 @@ class UserEntity(models.Model):
         db_table = 't_user'
         verbose_name = '用戶管理'
         verbose_name_plural = verbose_name
+
+class CategoryEntity(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 't_category'
+        verbose_name = '類別管理'
+        verbose_name_plural = verbose_name
+
+class DeviceEntity(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.IntegerField(verbose_name='價格')
+    vendor = models.CharField(max_length=30)
+    category = models.ForeignKey('CategoryEntity', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+    '''
+    Warning: By default on_delete=models.CASCADE, which means that if the author was deleted, 
+    this book would be deleted too! We use SET_NULL here, 
+    but we could also use PROTECT or RESTRICT to prevent the author being deleted while any book uses it.
+    '''
+
+    class Meta:
+        db_table = 't_device'
+        verbose_name = '設備管理'
+        verbose_name_plural = verbose_name
